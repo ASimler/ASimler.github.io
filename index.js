d6ba1127ce490bcebb92b051691bd802090e4e3a
ASimler <adelesimler96@gmail.com>
2025-04-09 16:54:10 +0200

    quizApp

--- index.js
+++ index.js
@@ -1,202 +1,119 @@
-const main = document.querySelector('main');
-const basicArray = [
-  {pic: 0, min: 1},
-  {pic: 1, min: 1},
-  {pic: 2, min: 1},
-  {pic: 3, min: 1},
-  {pic: 4, min: 1},
-  {pic: 5, min: 1},
-  {pic: 6, min: 1},
-  {pic: 7, min: 1},
-  {pic: 8, min: 1},
-  {pic: 9, min: 1},
+class Question {
+  constructor(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+  }
+
+  isCorrectAnswer(choice) {
+    return choice === this.answer;
+  }
+}
+
+const questions = [
+  new Question(
+    "Quelle méthode Javascript permet de filtrer les éléments d'un tableau",
+    ["indexOf()", "map()", "filter()", "reduce()"],
+    "filter()"
+  ),
+  new Question(
+    "Quelle méthode Javascript permet de vérifier si un élément figure dans un tableau",
+    ["isNaN()", "includes()", "findIndex()", "isOdd()"],
+    "includes()"
+  ),
+  new Question(
+    "Quelle méthode transforme du JSON en un objet Javascript ?",
+    ["JSON.parse()", "JSON.stringify()", "JSON.object()", "JSON.toJS"],
+    "JSON.parse()"
+  ),
+  new Question(
+    "Quel objet Javascript permet d'arrondir à l'entier le plus proche",
+    ["Math.ceil()", "Math.floor()", "Math.round()", "Math.random()"],
+    "Math.round()"
+  ),
 ];
 
-let exerciceArray = [];
-
-// Get stored exercices array
-// (Fonction qui se lance toute seule sans nom, ne se lance qu'une fois au load)
-(() => {
-  if (localStorage.exercices) {
-    exerciceArray = JSON.parse(localStorage.exercices);
-  } else {
-    exerciceArray = basicArray;
-  }
-})();
-
-
-class Exercice {
-  constructor() {
-    this.index = 0;
-    this.minutes = exerciceArray[this.index].min;
-    this.seconds = 0;
+class Quiz {
+  constructor(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.currentQuestionIndex = 0;
   }
 
-  uptadeCountdouwn() {
-    this.seconds = this.seconds < 10 ? "0" + this.seconds : this.seconds;
+  getCurrentQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    setTimeout(() => {
-      if (this.minutes === 0 && this.seconds === "00") {
-        this.index++;
-        this.ring();
-        if (this.index < exerciceArray.length) {
-          this.minutes = exerciceArray[this.index].min;
-          this.seconds = 0;
-          this.uptadeCountdouwn();
-        } else {
-          return page.finish();
-        }
-      } else if (this.seconds === "00") {
-        this.minutes--;
-        this.seconds = 59;
-        this.uptadeCountdouwn();
-      } else {
-        this.seconds--;
-        this.uptadeCountdouwn();
+  guess(answer) {
+    if (this.getCurrentQuestion().isCorrectAnswer(answer)) {
+      this.score++;
+    }
+    this.currentQuestionIndex++;
+  }
+
+  hasEnded() {
+    return this.currentQuestionIndex >= this.questions.length;
+  }
+}
+
+// Quizz display
+const display = {
+  elementShown: function(id, text) {
+    let element = document.getElementById(id);
+    element.innerHTML = text;
+  },
+
+  question: function() {
+    this.elementShown('question', quiz.getCurrentQuestion().text);
+  },
+
+  choices: function() {
+    let choices = quiz.getCurrentQuestion().choices;
+
+    guessHandler = (id, guess) => {
+      // onclick car on ne peut pas mettre de addEventListener dans un objet
+      document.getElementById(id).onclick = function () {
+        quiz.guess(guess);
+        quizApp();
       }
-    }, 1000)
-
-    return (main.innerHTML = `
-    <div class="exercice-container">
-      <p>${this.minutes}:${this.seconds}</p>
-      <img src="./img/${exerciceArray[this.index].pic}.png" />
-      <div>${this.index + 1}/${exerciceArray.length}</div>
-    </div>
-    `)
-  }
-
-  ring() {
-    const audio = new Audio();
-    audio.src = "ring.mp3";
-    audio.play();
-  }
-}
-
-
-const utils = {
-  // Pour éviter de répéter des innerHTML à chaque nouvelle vue de la page, on crée une fonction :
-  pageContent: function(title, content, btn) {
-    document.querySelector('h1').innerHTML = title;
-    main.innerHTML = content;
-    document.querySelector('.btn-container').innerHTML = btn;
+    }
+    // Affichage des choix et prise en compte du choix de la réponse
+    for (let i = 0; i < choices.length; i++) {
+      this.elementShown("choice" + i, choices[i]);
+      guessHandler('guess' + i, choices[i]);
+    }
   },
 
-  handleEventMinutes: function() {
-    document.querySelectorAll('input[type="number"]').forEach((input) => {
-      input.addEventListener('input', (e) => {
-        exerciceArray.map((exo) => {
-          if (exo.pic == e.target.id) {
-            exo.min = parseInt(e.target.value);
-            this.store();            
-          }
-        }) 
-      })
-    })
+  progress: function() {
+    this.elementShown('progress', `Question ${quiz.currentQuestionIndex + 1} sur ${quiz.questions.length}`);
   },
 
-  handleEventArrow: function() {
-    document.querySelectorAll('.arrow').forEach((arrow) => {
-      arrow.addEventListener('click', (e) => {
-        let position = 0;
-        exerciceArray.map((exo) => {
-          if (exo.pic == e.target.dataset.pic && position !== 0) {
-            [exerciceArray[position], exerciceArray[position -1]] = [exerciceArray[position -1], exerciceArray[position]];
-            page.lobby();
-            this.store();
-          } else {
-            position++;
-          }
-        }) 
-      })
-    })
-  },
-
-  deleteItem: function() {
-    document.querySelectorAll('.deleteBtn').forEach((btn) => {
-      btn.addEventListener('click', (e) => {
-        let newArr = [];
-        exerciceArray.map((exo) => {
-          if (exo.pic != e.target.dataset.pic) {
-            newArr.push(exo);
-          }
-        })
-        exerciceArray = newArr;
-        page.lobby();
-        this.store();  
-      })
-    })
-  },
-
-  reboot: function() {
-    exerciceArray = basicArray;
-    page.lobby();
-    this.store();
-  },
-
-  store: function() {
-    localStorage.exercices = JSON.stringify(exerciceArray);
+  endQuiz: function() {
+    let endQuizHTML = `
+      <h1>Quiz terminé !</h1>
+      <h3>Votre score est de : ${quiz.score} / ${quiz.questions.length}</h3>
+    `
+    this.elementShown('quiz', endQuizHTML);
   }
 }
 
-
-const page = {
-  lobby: function() {
-    let mapArray = exerciceArray.map((exo) => {
-      // map avec accolades = RETURN ` !!!!
-      return `
-          <li>
-            <div class='card-header'>
-              <input type='number' id=${exo.pic} min='1' max='10' value=${exo.min}>
-              <span>min</span>
-            </div>
-            <img src='./img/${exo.pic}.png' />
-            <i class='fas fa-arrow-alt-circle-left arrow' data-pic=${exo.pic}
-            ></i>
-            <i class='fas fa-times-circle deleteBtn' data-pic=${exo.pic}
-            ></i>
-          </li>
-      `
-    }).join('');
-    utils.pageContent(
-      "Paramétrage <i id='reboot' class='fas fa-undo'></i>",
-      "<ul>" + mapArray + "</ul>",
-      "<button id='start'>Commencer<i class='far fa-play-circle'></i></button>"
-    );
-    utils.handleEventMinutes();
-    utils.handleEventArrow();
-    utils.deleteItem();
-    reboot.addEventListener('click', () => {
-      utils.reboot();
-    });
-    start.addEventListener('click', () => {
-      this.routine();
-    });
-  },
-
-  routine: function() {
-    const exercice = new Exercice();
-    utils.pageContent(
-      "Routine",
-      exercice.uptadeCountdouwn(),
-      null
-      // null car pas de bouton sur cette vue
-    )
-  },
-
-  finish: function() {
-    utils.pageContent(
-      "C'est terminé !",
-      "<button id='start'>Recommencer</button>",
-      "<button id='reboot' class='btn-reboot'>Réinitialiser <i class='fas fa-times-circle'</i></button>"
-    );
-
-    start.addEventListener('click', () => {
-      this.routine();
-    });
-    reboot.addEventListener('click', () => {
-      utils.reboot();
-    });
+// Game logic
+quizApp = () => {
+  if (quiz.hasEnded()) {
+    display.endQuiz();
+  } else {
+    display.question();
+    display.choices();
+    display.progress();
   }
 }
 
-page.lobby();
+// Create Quizz
+let quiz = new Quiz(questions);
+quizApp();
+
+
+
+
+
+
