d1ff308eaef5c449a13e55dcf15f12dc9d90c0f8
ASimler <adelesimler96@gmail.com>
2025-04-13 19:09:01 +0200

    website code tp

---
+++ index.js
@@ -0,0 +1,25 @@
+// Connaitre niveau de scroll (window.scrollY)
+const nav = document.querySelector("nav");
+console.log(nav);
+
+
+// On l'implémente une première fois avec la valeur initiale
+let scrollValue = window.scrollY;
+
+window.addEventListener("scroll", (e) => {
+  console.log(scrollValue);
+
+  // Une fois le scroll commencé on le compare avec la variable scrollValue
+  if (window.scrollY < scrollValue) {
+    nav.style.top = "20px";
+    nav.style.background = "var(--white2)";
+    nav.style.width = "80%";
+  } else {
+    nav.style.top = "0px";
+    nav.style.background = "var(--color2)";
+    nav.style.width = "100%";
+  }
+
+  // Une fois le test terminé on réimplémente scrollValue avec la nouvelle valeur du scrollY
+  scrollValue = window.scrollY;
+});
